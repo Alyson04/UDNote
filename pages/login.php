07d4ced1954f3fff0220461a1edc5f4da341159f
alyson04 <alysoncalimag@gmail.com>
2024-08-12 16:05:39 +0800

    create a login and signup branch later

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+
+if (isset($_SESSION['ID'])) {
+    header("Location: index.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,17 +14,21 @@
     <title>Login</title>
 </head>
 <body>
+    <div class="toast-container" id="toast-container"></div>
+    
     <h2>Login</h2>
     <form action="authenticate.php" method="POST">
         <label for="email">Email:</label><br>
-        <input type="email" name="email" required><br><br>
+        <input autocomplete="on" type="email" name="email" required><br><br>
         
         <label for="password">Password:</label><br>
-        <input type="password" name="password" required><br><br>
+        <input autocomplete="off" type="password" name="password" required><br><br>
         
         <button type="submit">Login</button>
     </form>
 
     <a href="signup.php">Signup</a>
+
+    <script src="../script/script.js"></script>
 </body>
 </html>
