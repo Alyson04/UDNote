a140cdf889e01719a552bdb23dbf114f8e5fc0e1
aacdnrz <aacdnrz@gmail.com>
2024-08-13 09:45:18 +0800

    w images

--- pages/home.php
+++ pages/home.php
@@ -1,61 +1,61 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-	<meta charset="utf-8">  
-	<title>U'D NOTE</title>
-	<link rel="stylesheet" href="../styles//home.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-  </head>
-  <body>
-    
-  <header>
-  <div class="sidebar" id="sidebar">
-   <img src="" alt="Profile Picture" class="profile-pic" onclick="toggleSidebar()">
-            <nav class="sidebar-nav">
-                <a href="#edit-profile" class="nav-item">Edit Profile</a>
-                <a href="#logout" class="nav-item">Log Out</a>
-            </nav>
+<head>
+    <meta charset="utf-8">
+    <title>U'D NOTE</title>
+    <link rel="stylesheet" href="../styles/home.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+</head>
+<body>
+<header>
+    <div class="sidebar" id="sidebar">
+        <img src="../assets/jk.jpg" alt="Profile Picture" class="profile-pic" onclick="toggleSidebar()">
+        <nav class="sidebar-nav">
+            <a href="#edit-profile" class="nav-item">Edit Profile</a>
+            <a href="#logout" class="nav-item">Log Out</a>
+        </nav>
+    </div>
+    <div class="header-content" id="header-content">
+        <div class="title">
+            <h1>U'D NOTE</h1>
         </div>
-        <div class="header-content" id="header-content">
-            <div class="title">
-                <h1>U'D NOTE</h1>
-            </div>
         <div class="search-box">
-    	<div class="icon"><i class="uil uil-search"></i></div>
-    	<input type="text" id="search" placeholder="Search">
-      	</div>
-  </header>
-  
-	<div class="popup-box">
-  	<div class="popup">
-    	<div class="content">
-      	<header>
-        	<p></p>
-        	<i class="uil uil-times"></i>
-      	</header>
-      	<form action="#">
-        	<div class="row title">
-          	<label>Title</label>
-          	<input type="text" spellcheck="false">
-        	</div>
-        	<div class="row description">
-          	<label>Description</label>
-          	<textarea spellcheck="false"></textarea>
-        	</div>
-        	<button></button>
-      	</form>
-    	</div>
-  	</div>
-	</div>
-	<div class="wrapper">
-	<li class="add-box">
-  	<div class="icon"><i class="uil uil-plus"></i></div>
-  	<p>Add new note</p>
+            <div class="icon"><i class="uil uil-search"></i></div>
+            <input type="text" id="search" placeholder="Search">
+        </div>
+    </div>
+</header>
+
+<div class="popup-box">
+    <div class="popup">
+        <div class="content">
+            <header>
+                <p></p>
+                <i class="uil uil-times"></i>
+            </header>
+            <form action="#">
+                <div class="row title">
+                    <label>Title</label>
+                    <input type="text" spellcheck="false">
+                </div>
+                <div class="row description">
+                    <label>Description</label>
+                    <textarea spellcheck="false"></textarea>
+                </div>
+                <button></button>
+            </form>
+        </div>
+    </div>
 </div>
 
+<div class="wrapper">
+    <li class="add-box">
+        <div class="icon"><i class="uil uil-plus"></i></div>
+        <p>Add new note</p>
+    </li>
+</div>
 
-	<script src="../script/home.js"></script>
-    
-  </body>
+<script src="../script/home.js"></script>
+</body>
 </html>
