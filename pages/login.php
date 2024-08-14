f1ae1b7c699c6a381a9131160ad1774204e58d12
gabcdefgh <gabbbperez04@gmail.com>
2024-08-14 13:30:12 +0800

    need to fix

--- pages/login.php
+++ pages/login.php
@@ -3,20 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/login_css.css">
     <title>Login</title>
 </head>
 <body>
-    <h2>Login</h2>
+<div class = container>
+    <div class = left-column>
+    <h1> Welcome </h1>
+    <h3>Capture, organize, and access your <br> ideas effortlessly.</h3>
+
     <form action="authenticate.php" method="POST">
-        <label for="email">Email:</label><br>
+        <label for="email">EMAIL</label><br>
         <input type="email" name="email" required><br><br>
         
-        <label for="password">Password:</label><br>
-        <input type="password" name="password" required><br><br>
-        
-        <button type="submit">Login</button>
-    </form>
+        <label for="password">PASSWORD</label><br>
+        <input type="password" name="password" required><br>
 
-    <a href="signup.php">Signup</a>
+        <a href = "forgotpass.php"> Forgot Password?<br><br><br><br></a>
+        <button type="submit">SIGN IN</button>
+    </div>
+
+    <div class = right-column> 
+    <h2>NEW HERE?</h2> 
+    <h3>Why wait? Join us now and <br> experience the difference!</h3>
+    <a href="signup.php"> SIGN UP </a>
+    </div>
+
+</form>
+</div>
+
 </body>
 </html>
--- pages/signup.php
+++ pages/signup.php
@@ -3,32 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/signup_css.css">
     <title>Signup</title>
 </head>
 <body>
-    <div class="container">
-    <h2>Signup</h2>
-    <form action="signup.php" method="POST">
-        <label for="firstName">First Name:</label><br>
+<img src="../assets/try.png">
+
+    <h2>Welcome to U'D Note!</h2>
+    <form action="register.php" method="POST">
+
+        <label for="firstName">FIRST NAME</label><br>
         <input type="text" name="firstName" required><br><br>
 
-        <label for="lastName">Last Name:</label><br>
+        <label for="lastName">LAST NAME</label><br>
         <input type="text" name="lastName" required><br><br>
-        
-        <label for="email">Email:</label><br>
+
+        <label for="email">EMAIL</label><br>
         <input type="email" name="email" required><br><br>
         
-        <label for="password">Password:</label><br>
+        <label for="password">PASSWORD</label><br>
         <input type="password" name="password" required><br><br>
 
-        <label for="password">Confirm Password:</label><br>
-        <input type="password" name="confirmPassword" required><br><br>
-        
-        <button type="submit" name="submit">Register</button>
+        <label for="password">CONFIRM PASSWORD</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <div class = "btn-container">
+        <button type="submit">CREATE ACCOUNT</button><br>
+
+    <a href="login.php">Sign In</a>
     </form>
 
-    <a href="login.php">Login</a>
-
-    </div>
 </body>
 </html>
