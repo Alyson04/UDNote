ba21a99fc045130bfe0f6304d281507698dba882
gabcdefgh <gabbbperez04@gmail.com>
2024-08-14 14:59:11 +0800

    sign up css

--- pages/signup.php
+++ pages/signup.php
@@ -8,15 +8,20 @@
 </head>
 <body>
 <img src="../assets/try.png">
-
+<div class = "container"> 
     <h2>Welcome to U'D Note!</h2>
     <form action="register.php" method="POST">
 
+    <div class = "name">
+        <div> 
         <label for="firstName">FIRST NAME</label><br>
         <input type="text" name="firstName" required><br><br>
-
+        </div>
+        <div>
         <label for="lastName">LAST NAME</label><br>
         <input type="text" name="lastName" required><br><br>
+        </div>
+        </div>
 
         <label for="email">EMAIL</label><br>
         <input type="email" name="email" required><br><br>
@@ -27,11 +32,12 @@
         <label for="password">CONFIRM PASSWORD</label><br>
         <input type="password" name="password" required><br><br>
 
-        <div class = "btn-container">
         <button type="submit">CREATE ACCOUNT</button><br>
 
+    <h3> Already have an account? </h3>
     <a href="login.php">Sign In</a>
     </form>
 
+</div>
 </body>
 </html>
