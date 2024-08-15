d350a991f301c0537efec27b402708e797efdd37
alyson04 <alysoncalimag@gmail.com>
2024-08-15 16:20:02 +0800

    made some changes

--- pages/signup.php
+++ pages/signup.php
@@ -36,26 +36,29 @@ if (isset($_SESSION['user_id'])) {
             }
         ?>
 
-    <h2>Signup</h2>
+    <h2>Welcome to U'D Note!</h2>
     <form action="./_signup.php" method="POST">
-        <label for="fullName">Full Name:</label><br>
+        <label for="fullName">FULL NAME</label><br>
         <input autocomplete="off" type="text" name="fullName"><br><br>
 
-        <label for="username">Username:</label><br>
+        <label for="username">USERNAME</label><br>
         <input autocomplete="off" type="text" name="username"><br><br>
         
-        <label for="email">Email:</label><br>
+        <label for="email">EMAIL</label><br>
         <input autocomplete="off" type="email" name="email"><br><br>
         
-        <label for="password">Password:</label><br>
+        <label for="password">PASSWORD</label><br>
         <input autocomplete="off" type="password" name="password" minlength="8"><br><br>
 
-        <label for="password">Confirm Password:</label><br>
+        <label for="password">CONFIRM PASSWORD</label><br>
         <input autocomplete="off" type="password" name="confirmPassword" minlength="8"><br><br>
         
-        <button type="submit">Register</button>
+        <button type="submit">CREATE ACCOUNT</button>
+    
+    
     </form>
-    <a href="login.php">Login</a>
+    <h3> Already have an account? </h3>
+    <a href="login.php">Sign In</a>
     </div>
     
 
