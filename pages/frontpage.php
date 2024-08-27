a637438c9f8d7cdf3affb29fc5d3126acae95879
aacdnrz <aacdnrz@gmail.com>
2024-08-27 15:33:20 +0800

    okay

---
+++ pages/frontpage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>U'D NOTE</title>
+    <link rel="stylesheet" href="../styles/frontpage.css">
+</head>
+<body>
+    <img src="../assets/front.jpg" alt="background pic">
+
+    <div class="container">
+        <h1>U'D NOTE </h1>
+    </div>
+</body>
+</html>
