ff74e9f69ccc5f05cbc7bbb3a8ac60ca574882dc
gabcdefgh <gabbbperez04@gmail.com>
2024-08-28 14:45:26 +0800

    forgot&reset design

--- forgotpassword/forgotpass.php
+++ forgotpassword/forgotpass.php
@@ -3,16 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/forgotpass.css">
     <title>Document</title>
 </head>
 <body>
-    <!-- forgot_password.html -->
+<img src="../assets/hehe.jfif" alt="background pic">
+    <div class = "container">
+    <h2> Forget Your Password?</h2>
+    <p> Enter the Email associated with your account and we’ll <br> send you a link to reset your password </p>
 
 <form action="_sendresetlink.php" method="post">
-    <label for="email">Enter your email address:</label>
+    <label for="email">EMAIL</label>
     <input type="email" id="email" name="email" required>
-    <button type="submit">Submit</button>
+    <button type="submit">SUBMIT</button>
 </form>
+
+        <div class="signup"> 
+            <h3> Don't have an account? </h3>
+            <a href="../pages/signup.php"> SIGN UP </a>
+        </div>
+    </div>
 
 </body>
 </html>
